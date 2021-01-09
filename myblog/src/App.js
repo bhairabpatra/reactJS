2b7071ef155ee0b67279a7b04a9a7581c7d1695e
Bhairab Patra <bhairab@otpl.com>
2021-01-09 14:39:21 +0530

    Initial Push

---
+++ myblog/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from '.././src/Person/Person';
+function App() {
+  return (
+    <div className="App">
+        <h1>Hai i am Bhairab</h1>
+         <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ myblog/src/Person/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const person = () => {
+    return (
+    
+            <p> I am Bhairab I am BhairabI am BhairabI am BhairabI am BhairabI am Bhairab  </p>
+        
+        )};
+
+export default person;
